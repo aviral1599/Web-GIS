745ba58048249575a9e0e900de7370da6ad39e50
aviral1599 <aviralbatra1599@gmail.com>
2022-12-17 08:25:06 +0530

    application

---
+++ Application/GIS/map.py
@@ -0,0 +1,36 @@
+
+import random
+import pandas as pd
+import seaborn as sns
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import requests
+
+sns.set_style('whitegrid')
+
+def plotting():
+    url = 'http://127.0.0.1:3000/api'
+    r = requests.get(url)
+    df = pd.read_json(r.text)
+    print(df)
+    # print(df)
+
+    fp = "GIS\Indian_States.shp"
+    map_df = gpd.read_file(fp)
+    print(map_df.head())
+
+
+    map_df.plot()
+
+    merged = map_df.set_index('st_nm').join(df.set_index('State'))
+    print(merged.head())
+
+    # fig, ax = plt.subplots(1, figsize=(10, 6))
+    # ax.axis('off')
+    # ax.set_title('State Wise Crime', fontdict={'fontsize': '25', 'fontweight' : '3'})
+
+    # plot the figure
+    # merged.plot(column='Theft', cmap='YlGn', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True)
+    # plt.savefig('mgis2.png')
+    # plt.show()
+    return merged
---
+++ Application/main.py
@@ -0,0 +1,44 @@
+from flask import Flask,render_template ,request
+from GIS.map import plotting
+from GIS.tweets import tweet 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    options = ['Murder','Theft','Rape','Kidnap','Total']
+    selected = options[0]
+    tweets = tweet()
+    desc = tweets[0]
+    crimetype = tweets[1]
+    location = tweets[2]
+    return render_template("home.html",name = 'new_plot', url ='static\images\india.png',options = options,selected=selected,desc=desc,crimetype=crimetype,location=location)
+
+@app.route("/map",methods=['GET', 'POST'])
+def map():
+    crime = list(request.form.values())
+    p = plotting()
+    fig, ax = plt.subplots(1, figsize=(10, 6))
+    ax.axis('off')
+    ax.set_title(f'State Wise {crime[0]} Crime Data', fontdict={'fontsize': '25', 'fontweight' : '3'})
+    plt.rcParams['savefig.facecolor']='#6FEDD6'
+    p.plot(column=crime[0], cmap='viridis', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True)
+    plt.savefig('static\images\mew_plot.png',bbox_inches='tight',pad_inches=0.5)
+    options = ['Murder','Theft','Rape','Kidnap','Total']
+    selected = crime[0]
+    tweets = tweet()
+    desc = tweets[0]
+    crimetype = tweets[1]
+    location = tweets[2]
+    return render_template("home.html",name = 'new_plot', url ='static\images\mew_plot.png',options = options,selected=selected,desc=desc,crimetype=crimetype,location=location)
+    
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+    
+if __name__ == "__main__":
+    app.run(debug=True)
