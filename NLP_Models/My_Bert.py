19d7a524e581258126b49a3a89b85425c50c8a90
Shashwat Garg <gargshashwat007@gmail.com>
2023-01-17 19:52:40 +0530

    Bert Completed

---
+++ NLP_Models/My_Bert.py
@@ -0,0 +1,56 @@
+import torch
+import numpy as np
+
+from torch import nn
+
+class Dataset(torch.utils.data.Dataset):
+
+    def __init__(self, df, tokenizer, labels, label_column, labelled = True):
+        if not labelled:
+            self.labels = [labels[label] for label in df[label_column]]
+        else:
+            self.labels = df[label_column].tolist()
+        self.texts = [tokenizer(text, 
+                               padding='max_length', max_length = 512, truncation=True,
+                                return_tensors="pt") for text in df['text']]
+
+    def classes(self):
+        return self.labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def get_batch_labels(self, idx):
+        # Fetch a batch of labels
+        return np.array(self.labels[idx])
+
+    def get_batch_texts(self, idx):
+        # Fetch a batch of inputs
+        return self.texts[idx]
+
+    def __getitem__(self, idx):
+
+        batch_texts = self.get_batch_texts(idx)
+        batch_y = self.get_batch_labels(idx)
+
+        return batch_texts, batch_y
+
+class BertClassifier(nn.Module):
+
+    def __init__(self, model, dropout=0.5):
+
+        super(BertClassifier, self).__init__()
+
+        self.bert = model
+        self.dropout = nn.Dropout(dropout)
+        self.linear = nn.Linear(768, 5)
+        self.relu = nn.ReLU()
+
+    def forward(self, input_id, mask):
+
+        _, pooled_output = self.bert(input_ids= input_id, attention_mask=mask,return_dict=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        final_layer = self.relu(linear_output)
+
+        return final_layer
